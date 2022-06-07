45c09cc96dc991b311f64ab196c7fd2ac07b0fec
Benjamin <pkobenj41@gmail.com>
2022-06-07 11:38:29 +0300

    add another class model

--- Chem/models.py
+++ Chem/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -9,4 +10,15 @@ class Profile(models.Model):
         location=models.CharField(max_length=200, blank=True)
         contact = models.CharField(max_length=30, blank=True)
     
+# class medicine(models.Model):
+#     name = models.CharField(max_length=50)
+#     price = models.DecimalField(max_digits=10)
+#     pharmacyName=models.CharField(max_length=50)
+#     location=models.CharField(max_length=50)
+
+class Pharmacy(models.Model):
+    name=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
+    medicine_name=models.CharField(max_length=50)
+    price=models.DecimalField(max_digits=10)
     
