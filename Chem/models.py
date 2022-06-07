37a8c68eccf34d7f6649e99679f7226feedcf10e
Benjamin <pkobenj41@gmail.com>
2022-06-07 15:05:16 +0300

    add changes

--- Chem/models.py
+++ Chem/models.py
@@ -1,7 +1,8 @@
 from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
 class Profile(models.Model):
         user = models.OneToOneField(User, on_delete=models.CASCADE)
